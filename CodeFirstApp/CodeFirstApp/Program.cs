f080d94ad66c7f3944c970737c930fd589fd771e
Braydon <burrow.braydon14@gmail.com>
2020-06-28 22:01:56 -0600

    Assignment 368

---
+++ CodeFirstApp/CodeFirstApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+           
+            
+                using (var db = new TheatresContext())
+                {
+                    // Create and save a new Blog
+                    Console.Write("Enter a name for a new Theatre: ");
+                    var name = Console.ReadLine();
+
+                    var theatre = new Theatre { Name = name };
+                    db.Theatres.Add(theatre);
+                    db.SaveChanges();
+
+                    // Display all Blogs from the database
+                    var query = from b in db.Theatres
+                                orderby b.Name
+                                select b;
+
+                    Console.WriteLine("All theatres in the database:");
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(item.Name);
+                    }
+
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    public class Theatre
+    {
+        public int TheatreId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Movie> Movies { get; set; }
+    }
+
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int TheatreId { get; set; }
+        public virtual Theatre Theatre { get; set; }
+    }
+    public class TheatresContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Theatre> Theatres { get; set; }
+    }
+}
+
+
+     
+
---
+++ Scores/Scores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Scores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in your first name.");
+            string date = DateTime.Today.ToShortDateString();
+            string uName = Console.ReadLine();
+            string msg = $"\nWelcome back {uName}. Today is {date}";
+            Console.WriteLine(msg);
+
+            string path = @"C:\Users\burro\Desktop\Big C# Projects\The-Tech-Academy-C-Sharp-Coding-Projects\Scores\Scores\studentsScores.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+            
+            double totalS = 0.0;
+
+            Console.WriteLine("\nStudent Score: \n");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\n " + line);
+                double score = Convert.ToDouble(line);
+                totalS += score;
+            }
+
+            double avgScore = totalS / lines.Length;
+            Console.WriteLine("\nTotal of " + lines.Length + " students scores. \tAverage score: " + avgScore);
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
